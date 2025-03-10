11a74efab12b1a0f71c1c03648ffeb3c2ecbc0a9
venkatdamarla <venkatdamarla@gmail.com>
2025-03-10 15:05:12 +0530

    docker with K8s

--- docker.sh
+++ docker.sh
@@ -16,12 +16,12 @@ systemctl enable docker
 usermod -aG docker ec2-user
 
 # #kubectl installation
-# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# mv kubectl /usr/local/bin/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
 
 # #eksctl
-# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-# mv /tmp/eksctl /usr/local/bin
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /usr/local/bin
 
--- docker.tf
+++ docker.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-  instance_type          = "t3.micro"
+  instance_type          = "t3.micro"  
 
   # 20GB is not enough
   root_block_device {
